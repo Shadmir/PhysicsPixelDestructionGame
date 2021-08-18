94b6535d2eefebee90ea6f18abf38dcb2ac618e0
Shadmir <david@tynobryn.co.uk>
2021-08-18 23:53:47 +0100

    Class

---
+++ PhysicsPixelDestructionGame/Block.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhysicsPixelDestructionGame
+{
+    class Block
+    {
+        //block class is basically just a method to generate pixels with a set of locations relative to the block and a list to store them
+    }
+}
