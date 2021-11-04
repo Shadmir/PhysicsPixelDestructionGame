45abc8ffc4c37a60560c12b92b6bba83f5d2310f
David Vaughan <016645@brgs.org.uk>
2021-11-04 14:08:08 +0000

    added a collision angle, need to work out what to do with that.

--- PhysicsPixelDestructionGame/Projectile.cs
+++ PhysicsPixelDestructionGame/Projectile.cs
@@ -12,21 +12,54 @@ namespace PhysicsPixelDestructionGame
         public Sprite projectile;
         public Vector2 position;
         public Vector2 velocity;
+        public List<Pixel> pixels;
+        public Sprite sprite;
+        public float collisionAngle;
         public ProjectileType projectileType;
-        public Dictionary<ProjectileType, float> TntEquiv = new Dictionary<ProjectileType, float> { { ProjectileType.TNT, 1 }, { ProjectileType.C4, 30.6f }, { ProjectileType.Gunpowder, 0.7f }, { ProjectileType.Nuclear, 490245.5f }, { ProjectileType.Firework, 1.5f }, { ProjectileType.HolyHandGrenade, 50f }
-        };
+        public int width = 15;
+        public int height = 15;
+        public Dictionary<ProjectileType, float> TntEquiv = new Dictionary<ProjectileType, float> {{ ProjectileType.TNT, 1 }, { ProjectileType.C4, 30.6f }, { ProjectileType.Gunpowder, 0.7f }, { ProjectileType.Nuclear, 490245.5f }, { ProjectileType.Firework, 1.5f }, { ProjectileType.HolyHandGrenade, 50f }};
         public int mass;
 
-        public Projectile(ProjectileType type, int Mass, Vector2 Pos, Vector2 Vel) {
+        public Projectile(ProjectileType type, int Mass, Vector2 Pos, Vector2 Vel, List<Pixel> Pix) {
             projectileType = type;
             mass = Mass;
             position = Pos;
             velocity = Vel;
+            pixels = Pix;
+            height *= (mass / 10);
+            width *= (mass / 10);
         }
         public void Update(GameTime gameTime)
         {
             position += velocity;
             velocity.Y -= 1;
+            Rectangle projectileFuturePos = new Rectangle((int)(position.X + velocity.X), (int)(position.Y + velocity.Y), width, height);
+            foreach (Pixel pixel in pixels)
+            {
+                Rectangle pixelPos = new Rectangle((int)pixel.Position.X, (int)pixel.Position.Y, pixel.Width, pixel.Height);
+                Rectangle projectilePos = new Rectangle((int)position.X, (int)position.Y, width, height);
+                if (projectilePos.Intersects(pixelPos))
+                {
+                    
+                }
+                if (projectileFuturePos.Intersects(pixelPos))
+                {
+                    //need to find the angle at which i strike the pixel. then lose something like 85% of E_k and bounce depending on
+                    collisionAngle = (float)Math.Acos(Vector2.Dot(velocity, new Vector2(1, 0)) / Math.Sqrt((Math.Pow(velocity.X,2) + Math.Pow(velocity.Y, 2))));
+                }
+
+                //speculative contact ^
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch, GameTime gameTime)
+        {
+
+        }
+        public void Explode()
+        {
+            this.Dispose();
         }
     }
 }
